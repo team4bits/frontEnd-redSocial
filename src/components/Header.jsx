0b8d3156668428a6f7c13e05c8638094566a10fc
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2025-06-29 18:17:51 -0300

    Navbar funcional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,36 @@
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">AntiSocial Social Net</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">AntiSocial Social Net</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Inicio</Nav.Link>
+              <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             </Nav>
             <Nav>
+              <Nav.Link as={Link} to="/registro">
+                Registrarse
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Iniciar Sesión
+              </Nav.Link>
               <NavDropdown title="Nick Name" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
+                <NavDropdown.Item as={Link} to="/perfil">
                   Mi Cuenta
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
+                <NavDropdown.Item as={Link} to="/editar-perfil">
                   Editar Perfil
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
+                <NavDropdown.Item as={Link} to="/actividad">
                   Mi Actividad
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
+                <NavDropdown.Item as={Link} to="/logout">
                   Cerrar Sesión
                 </NavDropdown.Item>
               </NavDropdown>
