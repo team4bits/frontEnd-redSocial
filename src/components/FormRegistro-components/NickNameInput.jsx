79623572a34d1e7094e12a13beed037698a51699
VictorGonzalvez <victorricardo.gonzalvezchara@estudiantes.unahur.edu.ar>
2025-06-29 18:12:29 -0300

    Manejo del estado de nickname

--- src/components/FormRegistro-components/NickNameInput.jsx
+++ src/components/FormRegistro-components/NickNameInput.jsx
@@ -1,21 +1,35 @@
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+
 
 const NickNameInput = (props) => {
   // props.usuarios contiene la lista de usuarios registrados hasta el momento
   const { usuarios } = props;
-  //Cuando el usuario salga del input, se puede validar si el nickname ya existe
-  const handleBlur = (event) => {
-    const nickname = event.target.value;
-    const exists = usuarios.some(user => user.nickName === nickname);
-    if (exists) {
-      alert("El nickname ya está en uso. Por favor, elige otro.");
+  const [nickName, setNickName] = useState("");
+  const setNickname = (e) => {
+    // Aquí se puede agregar la lógica para validar el nickname
+    // Por ejemplo, verificar si ya existe en la lista de usuarios
+    const nicknameValue = e.target.value;
+    setNickName(nicknameValue);
+    if (usuarios.includes(nicknameValue)) {
+      console.warn("El nickname ya existe");
+    } else {
+      console.log("Nickname válido");
     }
-  }
-  return (
+  };
+
+  //Cuando el usuario salga del input, se puede validar si el nickname ya existe
+  return (<>
     <Form.Group className="mb-3" controlId="formBasicNickname">
       <Form.Label>Nickname</Form.Label>
-      <Form.Control type="text" placeholder="Ingresa un nickname" onBlur={handleBlur} />
+      <Form.Control type="text" placeholder="Ingresa un nickname" onChange={setNickname} />
+      <p>
+        {usuarios.includes(nickName) ? (
+          <span style={{ color: 'red' }}>El nickname ya existe</span>
+        ) : (<><span style={{ color: 'green' }}>Nickname disponible</span></>)
+        }</p>
     </Form.Group>
+    </>
   );
 };
 
