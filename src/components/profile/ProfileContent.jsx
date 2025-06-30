19de167bab8187a65538017e9ff57883ed2ad712
LSantana32 <lucasnahuel.santana@estudiantes.unahur.edu.ar>
2025-06-30 17:04:10 -0300

    se agrego el formPost en el perfil

--- src/components/profile/ProfileContent.jsx
+++ src/components/profile/ProfileContent.jsx
@@ -4,37 +4,40 @@ import Comment from '../Comment';
 import FormPost from '../FormPost';
 
 const ProfileContent = ({ activeTab, user }) => {
-  <FormPost />
-  if (activeTab === 'posts') {
-    return user.posts.length > 0 ? (
-      user.posts.map((post) => (
-        <Post 
-          key={post.id} 
-          fecha={post.fecha}
-          archives={post.archives}
-          comments={post.comments}
-        />
-      ))
-    ) : (
-      <Alert variant="info">No hay posts</Alert>
-    );
-  }
+  return (
+    <>
+      {activeTab === 'posts' && (
+        <>
+          <FormPost />
+          {user.posts.length > 0 ? (
+            user.posts.map((post) => (
+              <Post 
+                key={post.id} 
+                user={user}
+                post={post}
+              />
+            ))
+          ) : (
+            <Alert variant="info">No hay posts</Alert>
+          )}
+        </>
+      )}
 
-  if (activeTab === 'comments') {
-    return user.comments.length > 0 ? (
-      user.comments.map((comment) => (
-        <Comment 
-          key={comment.id} 
-          content={comment.content}
-          fecha={comment.fecha}
-        />
-      ))
-    ) : (
-      <Alert variant="info">No hay comentarios</Alert>
-    );
-  }
-
-  return null;
+      {activeTab === 'comments' && (
+        user.comments.length > 0 ? (
+          user.comments.map((comment) => (
+            <Comment 
+              key={comment.id} 
+              content={comment.content}
+              fecha={comment.fecha}
+            />
+          ))
+        ) : (
+          <Alert variant="info">No hay comentarios</Alert>
+        )
+      )}
+    </>
+  );
 };
 
 export default ProfileContent;
